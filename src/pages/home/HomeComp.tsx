18e5da21f4a4cdf0e3fe62841b73d5f231ba7975
stella omoniyi <stella29117173@gmail.com>
2026-06-30 16:28:40 -0200

    up

--- src/pages/home/HomeComp.tsx
+++ src/pages/home/HomeComp.tsx
@@ -3,7 +3,6 @@
 import About from "./About"
 import Card from "./Card"
 import Client from "./Client"
-// import Contact from "./Contact"
 import Core from "./Core"
 import Executive from "./Executive"
 import Hero from "./Hero"
@@ -24,7 +23,6 @@ const HomeComp = () => {
         <Client/>
         <Card/>
         <Executive/>
-        {/* <Contact/> */}
     </div>
   )
 }
